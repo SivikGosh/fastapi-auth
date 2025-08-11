bd79108e88c2e106f5cae73097c12e30ea505d55
Georgiy <37848058+SivikGosh@users.noreply.github.com>
2025-08-11 12:53:19 +0300

    0.1.2: Исправление alembic env для миграций

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from alembic import context
 
 from src.database import Base, db_url
+from src.users import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
