9674113947d3eab1243df725d0cc639f2d07459b
Georgiy <37848058+SivikGosh@users.noreply.github.com>
2025-08-12 10:01:59 +0300

    0.2.1: Перевод полей модели на mapped_column (современная реализация)

--- src/users/models.py
+++ src/users/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Boolean, Column, Integer, String
-from sqlalchemy.orm import validates
+from sqlalchemy import Boolean, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, validates
 
 from src.database import Base
 
@@ -9,12 +9,37 @@ from .validators import email, phone
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=True)
-    phone = Column(String, unique=True, index=True, nullable=True)
-    password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        index=True,
+    )
+    username: Mapped[str] = mapped_column(
+        String,
+        unique=True,
+        index=True,
+        nullable=False,
+    )
+    email: Mapped[str] = mapped_column(
+        String,
+        unique=True,
+        index=True,
+        nullable=True,
+    )
+    phone: Mapped[str] = mapped_column(
+        String,
+        unique=True,
+        index=True,
+        nullable=True,
+    )
+    password: Mapped[str] = mapped_column(
+        String,
+        nullable=False,
+    )
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True,
+    )
 
     @validates('email')
     def validate_email(self, field, value):
